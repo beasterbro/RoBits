b8999a552d356aa9e2cce52121e3dc06c0e09443
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-10-10 14:47:18 -0400

    Added basic menu navigation and battle scene
    added main menu, team selection menu for battle, bot selection menu for customizing, team selection menu for training, and currently working on inventory integration

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
