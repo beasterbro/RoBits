71da87867f563e0c9537d181ea0223c020ed8f7f
Lucas Popp <lmp122@case.edu>
2019-10-12 22:31:52 -0400

    Began adding serialization for BotInfo and TeamInfo

--- Assets/Scripts/BotInfo.cs
+++ Assets/Scripts/BotInfo.cs
@@ -1,6 +1,20 @@
-﻿// TODO: Add custom serializer
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Add custom serializer
 public class BotInfo
 {
+
+    private class DbBot
+    {
+        public int bid;
+        public string name;
+        public int[] parts;
+        public int tier;
+        public Dictionary<string, string> ai; // TODO: Change to a better data type later
+    }
+
+    
     
     // botBehavior
     // equippedParts
