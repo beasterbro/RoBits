a3ed60c6c6578cf1003584291af78ccdffb6ced5
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-10-15 20:26:35 -0400

    A class for generating images
    it pulls the image from the database to assign it to part images

---
+++ Assets/ImageGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class ImageGenerator : MonoBehaviour
+{
+
+    public List<Image> gatheredImages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Image image in gatheredImages)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
