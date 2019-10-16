b094a629724ca2841cbd7706aacf1bb41bef85d8
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-10-15 20:20:43 -0400

    Adding item component
    represents an InventoryItem in unity

---
+++ Assets/Scripts/InventoryScene/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+
+/*
+ * Represents a part while the user is in the inventory
+ */
+public class Item : MonoBehaviour
+{
+    
+    [SerializeField] public Image partImage;
+    private InventoryItem inventoryItem;
+    public PartInfo partInfo ;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        partInfo = inventoryItem.GetPart();
+        partImage.name = partInfo.GetName();
+    }
+}
