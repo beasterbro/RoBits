190918e3b96176d590f08b960824d45e689c9dd3
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-10-21 19:40:21 -0400

    working on ImageGenerator

--- Assets/ImageGenerator.cs
+++ Assets/ImageGenerator.cs
@@ -10,7 +10,7 @@ public class ImageGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        foreach (Image image in gatheredImages)
+//        foreach (Image image in gatheredImages)
         {
             
         }
