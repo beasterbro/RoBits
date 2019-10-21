911d625c80c1ec177f8c7ecdcbc582d260518864
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-10-21 19:39:14 -0400

    Base setup for testing suite
    mostly setup on the unity side

---
+++ Assets/RW/Tests/TestSuite.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        private static Dictionary<string, double> attributes = null;
+        private static Sprite sprite;
+        private static PartInfo bodyType = new PartInfo(1,"body","The body",PartType.BodyType,100,1,false,attributes,sprite);
+        private static PartInfo weapon = new PartInfo(1,"weapon","The weapon",PartType.Weapon,100,1,false,attributes,sprite);
+        private static PartInfo sensor = new PartInfo(1,"sensor","The senson",PartType.Sensor,100,1,false,attributes,sprite);
+        private static List<PartInfo> equipment = new List<PartInfo>(new PartInfo[] {weapon,sensor});
+        
+        BotInfo _botInfo = new BotInfo(1,"test",0,equipment,bodyType);
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestSuiteSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.That(_botInfo != null);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestSuiteWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
