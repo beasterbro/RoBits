20fb0d22b45b9af1c109783698ea3e252ee0f238
Lucas Popp <lucaspopp0@gmail.com>
2019-10-21 01:14:10 -0400

    Set up JSON parsing the right way (#1)
    * Installed Newtonsoft JSON.NET package for JSON parsing
    
    * Added abstract JSON Converter class, and helper classes for serialization/deserialization
    
    * Switched form Unity serialization to Newtonsoft
    
    * Cleaned up imports
    
    * Added comments, and simplified some code using convenience methods in DataManager
    
    * Moved JSON classes to their own namespace, and added some methods to DataManager for updating
    
    * Added setter methods
    
    * Reorganize DataManager methods and add update functions
    
    * Fixed bad method invocation

---
+++ Assets/Scripts/Data/InventoryItem.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+[JsonConverter(typeof(JsonData.InventoryItemConverter))]
+public class InventoryItem
+{
+
+    private PartInfo part;
+    private int count;
+
+    public InventoryItem(PartInfo part, int count)
+    {
+        this.part = part;
+        this.count = count;
+    }
+
+    public PartInfo GetPart()
+    {
+        return part;
+    }
+
+    public int GetCount()
+    {
+        return count;
+    }
+
+    public void IncreaseCount()
+    {
+        count++;
+    }
+
+    public bool DecreaseCount()
+    {
+        if (count > 0)
+        {
+            count--;
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/Data/Json Converters/SerializationHelper.cs
@@ -0,0 +1,48 @@
+using Newtonsoft.Json;
+
+namespace JsonData
+{
+    // Adds convenience methods for converting to JSON
+    public class SerializationHelper
+    {
+
+        private JsonWriter writer;
+        private JsonSerializer serializer;
+
+        public SerializationHelper(JsonWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public SerializationHelper(JsonWriter writer, JsonSerializer serializer)
+        {
+            this.writer = writer;
+            this.serializer = serializer;
+        }
+
+        public void Start()
+        {
+            writer.WriteStartObject();
+        }
+
+        public void End()
+        {
+            writer.WriteEndObject();
+        }
+
+        // Writes a key/value pair
+        public void WriteKeyValue<T>(string key, T value)
+        {
+            writer.WritePropertyName(key);
+            writer.WriteValue(value);
+        }
+
+        // Writes a key/value pair for a non-primitive value, which must be serialized
+        public void SerializeKeyValue<T>(string key, T value)
+        {
+            writer.WritePropertyName(key);
+            serializer.Serialize(writer, value);
+        }
+
+    }
+}
