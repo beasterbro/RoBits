03a41d79df868636c56f6e4edc9fecc27845bcd9
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-10-22 20:24:13 -0400

    instantiating required info for testing

--- Assets/RW/Tests/TestSuite.cs
+++ Assets/RW/Tests/TestSuite.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Rider.Unity.Editor.NonUnity;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -8,20 +10,54 @@ namespace Tests
 {
     public class TestSuite
     {
-        /*private static Dictionary<string, double> attributes = null;
-        private static Sprite sprite;
-        private static PartInfo bodyType = new PartInfo(1,"body","The body",PartType.BodyType,100,1,false,attributes,sprite);
-        private static PartInfo weapon = new PartInfo(1,"weapon","The weapon",PartType.Weapon,100,1,false,attributes,sprite);
-        private static PartInfo sensor = new PartInfo(1,"sensor","The senson",PartType.Sensor,100,1,false,attributes,sprite);
-        private static List<PartInfo> equipment = new List<PartInfo>(new PartInfo[] {weapon,sensor});
-        */
-        //BotInfo _botInfo = new BotInfo(1,"test",0,equipment,bodyType);
-        // A Test behaves as an ordinary method
+       private static Dictionary<String,double> attributes1 = new Dictionary<string, double>
+        {
+            {"DMG",23.0},
+            {"DIST",12}
+        };
+       
+       private static Dictionary<String,double> attributes2 = new Dictionary<string, double>
+       {
+           {"DMG",23.0},
+           {"DIST",12}
+       };
+       
+       private static Dictionary<String,string> settings = new Dictionary<string, string>
+       {
+           {"DARK","yes"},
+           {"LOUD","no"}
+       };
+       
+       private static Dictionary<String,double> bodySpec = new Dictionary<string, double>
+       {
+           {"THICC",11},
+           {"SANIC",101}
+       };
+       
+       
+        private static PartInfo part1 = new PartInfo(0, "0", "first part", PartType.Weapon, 1, 1, true, attributes1);
+        private static PartInfo part2 = new PartInfo(1, "1", "second part", PartType.Weapon, 2, 2, true, attributes2);
+
+        private static List<PartInfo> allParts = new List<PartInfo>(new PartInfo[]{part1,part2});
+        
+        private static PartInfo body = new PartInfo(2, "body", "thrid part", PartType.BodyType, 2, 2, false, bodySpec);
+
+        private UserInfo _userInfo = new UserInfo("testUser","ass@ass.com","tester101",100,200,true,settings);
+       
+        private static BotInfo bot0 = new BotInfo(0,"bot0",0,allParts,body);
+        private static BotInfo bot1 = new BotInfo(1,"bot1",1,allParts,body);
+        private static BotInfo bot2 = new BotInfo(2,"bot2",2,allParts,body);
+
+        private static BotInfo[] botTeam = new[] {bot0, bot1, bot2};
+        
+        
+        /*
+         * Testing for InventoryController
+         */
         [Test]
         public void TestingBotInfoAssigned()
         {
-            // Use the Assert class to test conditions
-            //Assert.That(_botInfo != null);
+            
         }
 
         [Test]
