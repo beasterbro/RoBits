0bc80b6974e759a6d18074e7f7aefa492df76a98
Jackson Nelson-Gal <jjn58@case.edu>
2019-10-24 04:37:02 -0400

    Added constant scale controller for objects that shouldn't scale.

---
+++ Assets/Scripts/BehaviorLab/Scaling/ConstantScaleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Interface Objects/Scale Controllers/Constant")]
+public class ConstantScaleController : ScaleController
+{
+    [SerializeField] private ScaleController onScale;
+
+    protected override void ApplyScale(Vector2 scale)
+    {
+        // Don't update scale, just use current scale
+        onScale.UpdateScale(this.Scale());
+    }
+}
