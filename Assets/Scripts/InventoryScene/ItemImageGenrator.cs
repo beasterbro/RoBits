2f551d1a5266b9e7d6983a5430cb0911ac5b517c
beasterbro <43251102+beasterbro@users.noreply.github.com>
2019-10-27 15:04:57 -0400

    Updating name of parts to part ID and adding image generator

---
+++ Assets/Scripts/InventoryScene/ItemImageGenrator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemImageGenrator : MonoBehaviour
+{
+    [SerializeField] List<Sprite> partImages;
+
+    public Sprite generateImage(int partID)
+    {
+        foreach (var sprite in partImages)
+        {
+            if (sprite.name == partID.ToString())
+            {
+                return sprite;
+            }
+        }
+
+        return null;
+    }
+}
