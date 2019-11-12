c63b55f654ccd351eb48687b7f680c77184bd4d9
Jackson Nelson-Gal <jjn58@case.edu>
2019-11-12 01:22:11 -0500

    Added basic storage structure for behaviorlab information.

---
+++ Assets/Scripts/Data/BehaviorInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorInfo
+{
+
+    private readonly int triggerId;
+    private int entryBlockId; // TODO: Either have "Entry Block" be a thing that is just a chunk (a.k.a. {...}) OR switch this to be int[] since the trigger starts with a chunk component.
+    private BlockInfo[] blocks;
+
+    public BehaviorInfo(int triggerId, int entryBlockId, BlockInfo[] blocks)
+    {
+        this.triggerId = triggerId;
+        this.entryBlockId = entryBlockId;
+        this.blocks = blocks;
+    }
+
+    public int TriggerId => triggerId;
+    public int EntryBlockId
+    {
+        get => entryBlockId;
+        set => entryBlockId = value;
+    }
+    public BlockInfo[] Blocks
+    {
+        get => blocks;
+        set => blocks = value;
+    }
+}
