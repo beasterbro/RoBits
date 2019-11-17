743f4d58335d7dc248d70288193787f1ff8d1e27
Lucas Popp <lmp122@case.edu>
2019-11-17 12:58:34 -0500

    Updated default body type value

--- Assets/Scripts/Data/Json Converters/BotConverter.cs
+++ Assets/Scripts/Data/Json Converters/BotConverter.cs
@@ -22,7 +22,7 @@ namespace JsonData
             var id = helper.GetValue<int>("bid");
             var name = helper.GetValue("name", "");
             var partIds = helper.GetValue("parts", new int[0]);
-            var bodyTypeId = helper.GetValue("bodyType", 0);
+            var bodyTypeId = helper.GetValue("bodyType", 100);
             var tier = helper.GetValue("tier", 0);
             var behaviors = helper.GetArrayValue<BehaviorInfo>("ai");
 
