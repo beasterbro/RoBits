736325e94810df5f96e1387a72ac9a5e34f1853d
Lucas Popp <lucaspopp0@gmail.com>
2019-11-24 17:55:25 -0500

    Fixed bug in DeserializationHelper causing memory errors in builds (#20)

--- Assets/Scripts/Data/Json Converters/DeserializationHelper.cs
+++ Assets/Scripts/Data/Json Converters/DeserializationHelper.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using UnityEngine;
 
 namespace JsonData
 {
@@ -10,16 +12,25 @@ namespace JsonData
     public class DeserializationHelper
     {
 
-        private JObject item;
+        private readonly JObject item;
 
         public DeserializationHelper(JObject item)
         {
             this.item = item;
         }
 
+        private bool HasValue(string key)
+        {
+            return item[key] != null;
+        }
+
         // Directly get a value from the JSON blob
         public T GetValue<T>(string key)
         {
+            // Confirm the item has the given key
+            if (!HasValue(key)) 
+                throw new KeyNotFoundException("Attempted to get value for missing key: '" + key + "'");
+
             // If not a plain value, serialize
             if (item[key].GetType() == typeof(JObject) || item[key].GetType() == typeof(JArray))
             {
@@ -30,31 +41,29 @@ namespace JsonData
             return item[key].Value<T>();
         }
 
-        // Wrapper for the above method, providing a default fallback if the property
-        // does not exist
+        // Wrapper for the above method, providing a default fallback if the property does not exist
         public T GetValue<T>(string key, T defaultValue)
         {
+            if (!HasValue(key)) return defaultValue;
+            
             try
             {
                 return GetValue<T>(key);
             }
-            catch (NullReferenceException e)
+            catch (Exception e)
             {
+                Debug.LogError(e);
                 return defaultValue;
             }
         }
 
-        public T[] GetArrayValue<T>(string key)
-        {
-            return GetArrayValue<T>(key, new T[0]);
-        }
+        public T[] GetArrayValue<T>(string key) => GetArrayValue(key, new T[0]);
 
         public T[] GetArrayValue<T>(string key, T[] defaultValue)
         {
             if (item[key].GetType() == typeof(JArray))
             {
-                return item[key].Children()
-                    .Select(token => JsonConvert.DeserializeObject<T>(token.ToString())).ToArray();
+                return item[key].Children().Select(token => JsonConvert.DeserializeObject<T>(token.ToString())).ToArray();
             }
 
             return defaultValue;
