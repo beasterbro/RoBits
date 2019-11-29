8bbbee6ebd039f9e6eb98625a25add2c29183c9b
Jackson Nelson-Gal <jjn58@case.edu>
2019-11-28 23:05:01 -0800

    Added dropdown component and move action block.

---
+++ Assets/Scripts/BehaviorLab/Blocks/DropdownBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Interface Objects/Blocks/Dropdown")]
+public abstract class DropdownBlock : ActionBlock
+{
+    // TODO: need to add current dropdown value to attributes of block during state creation!
+    [SerializeField] private DropdownComponent dropdown;
+
+    protected override void Start()
+    {
+        base.Start();
+        UpdateItems();
+    }
+
+    public void UpdateItems()
+    {
+        dropdown.SetSupplier(Supplier);
+    }
+
+    protected abstract List<string> Supplier();
+}
---
+++ Assets/Scripts/BehaviorLab/Blocks/MoveBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : DropdownBlock
+{
+    public override bool IsValid()
+    {
+        foreach (PartInfo part in BehaviorLabController.GetShared().currentBot.Equipment)
+        {
+            if (PartType.Transport == part.PartType) return true;
+        }
+        return false;
+    }
+
+    protected override List<string> Supplier()
+    {
+        return new List<string>{ "forward", "backward", "up", "down" }; // Forward/backward as opposed to left/right since teams could be on either side
+    }
+
+    protected override string Type() => "Move";
+}
