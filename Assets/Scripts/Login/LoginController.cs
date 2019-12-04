78691e710cf954c9876134b57334fd519258026e
Lucas Popp <lmp122@case.edu>
2019-12-04 00:37:35 -0500

    Added UI for user creation

--- Assets/Scripts/Login/LoginController.cs
+++ Assets/Scripts/Login/LoginController.cs
@@ -1,14 +1,22 @@
 ﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoginController : MonoBehaviour
 {
+
+    public GameObject userCreationPanel;
+    public InputField usernameInput;
+    public Text errorLabel;
 
     private bool userLoggedIn;
     private string googleToken;
     private bool isEstablishingAuth;
 
+    private string newUserEmail;
+    
     #if UNITY_WEBGL && !UNITY_EDITOR
     [DllImport("__Internal")]
     private static extern void ShowLoginButton();
@@ -31,13 +39,14 @@ public class LoginController : MonoBehaviour
 
     private void Start()
     {
+        userCreationPanel.SetActive(false);
         ShowLoginButton();
         DataManager.Instance.Latch(this);
     }
 
     private void Update()
     {
-        #if UNITY_EDITOR
+        #if false && UNITY_EDITOR
         if (!isEstablishingAuth)
         {
             isEstablishingAuth = true;
@@ -49,11 +58,21 @@ public class LoginController : MonoBehaviour
         {
             HideLoginButton();
             isEstablishingAuth = true;
-            StartCoroutine(DataManager.Instance.EstablishAuth(googleToken, success =>
+            
+            StartCoroutine(DataManager.Instance.EstablishAuth(googleToken, (success, email) =>
             {
                 if (success)
                 {
-                    SceneManager.LoadScene(Scenes.Menu);
+                    StartCoroutine(DataManager.Instance.EmailIsTaken(email, isExistingUser =>
+                    {
+                        if (isExistingUser) SceneManager.LoadScene(Scenes.Menu);
+                        else
+                        {
+                            newUserEmail = email;
+                            errorLabel.gameObject.SetActive(false);
+                            userCreationPanel.SetActive(true);
+                        }
+                    }));
                 }
                 else
                 {
@@ -65,6 +84,37 @@ public class LoginController : MonoBehaviour
             }));
         }
         #endif
+    }
+
+    public void AttemptJoin()
+    {
+        var username = usernameInput.text.Trim();
+
+        if (username.Length == 0 || Regex.IsMatch(username, "\\W"))
+        {
+            errorLabel.gameObject.SetActive(true);
+            errorLabel.text = "Invalid username";
+        }
+        else
+        {
+            errorLabel.gameObject.SetActive(false);
+            StartCoroutine(DataManager.Instance.UsernameIsTaken(username, isTaken =>
+            {
+                if (isTaken)
+                {
+                    errorLabel.gameObject.SetActive(true);
+                    errorLabel.text = "Username taken";
+                }
+                else
+                {
+                    StartCoroutine(DataManager.Instance.CreateUser(username, newUserEmail, success =>
+                    {
+                        if (!success) return;
+                        SceneManager.LoadScene(Scenes.Menu);
+                    }));
+                }
+            }));
+        }
     }
 
 }
